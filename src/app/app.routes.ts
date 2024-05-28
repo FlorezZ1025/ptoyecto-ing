1fdd13e8420203a30af086291f408ad924c2eb24
Daniel Lasso <dlassoc123@gmail.com>
2024-05-28 00:26:10 -0500

    Mis contratos view, AYUDA NO SE CENTRAR ESO AJAJJA

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { UserComponent } from './components/user/user.component';
-import { ContratosComponent } from './components/contratos/contratos.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { AboutInfoComponent } from './components/about-info/about-info.component';
@@ -10,7 +9,6 @@ export const routes: Routes = [
     {
         path: '' , component: HomeComponent },
     {   path: 'login', component:LoginComponent},
-    {   path: 'contratos', component: ContratosComponent},
     {   path: 'about', component:AboutInfoComponent},
     {   path: 'mis-contratos', component: MisContratosComponent},
     {   path: '**',  redirectTo:''}
